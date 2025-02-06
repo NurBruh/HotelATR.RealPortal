7372be7b6004ae6a3fc053562130904efd3f5466
NurBruh <amdfx9590@bk.ru>
2025-02-06 08:54:29 +0500

    Добавлено ивенты

---
+++ HotelATR.RealPortal/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using HotelATR.RealPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelATR.RealPortal.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            #region GET data from DataBase
+
+            List<EventViewModel> events = new List<EventViewModel>();
+            events.Add(new EventViewModel()
+            {
+                ImageSize = 2,
+                PathImage = "~/img/event/1.jpg",
+                AuthorName = "By : Jhtyg Hyuir",
+                EventTitle = "management conferences",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt",
+            });
+
+
+
+            #endregion
+
+
+            return View(events);
+        }
+    }
+}
---
+++ HotelATR.RealPortal/Models/EventViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelATR.RealPortal.Models
+{
+    public class EventViewModel
+    {
+        //imgpath
+        public string PathImage { get; set; }
+        public int ImageSize { get; set; }
+        //Billy Herrington
+        public string AuthorName { get; set; }
+        //(Bruh)
+        public string EventTitle { get; set; }
+        //Bonjour messie slave.
+        public string Description { get; set; }
+        //Yapiiiiiiii
+    }
+}
