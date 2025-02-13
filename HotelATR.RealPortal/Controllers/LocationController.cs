4033a8e2482a127e4607705f642d0a9cefcf2356
NurBruh <amdfx9590@bk.ru>
2025-02-13 09:02:07 +0500

    япи

---
+++ HotelATR.RealPortal/Controllers/LocationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelATR.RealPortal.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
